8945de5663bfc21d2cea0d59872d000f5206e77f
smartgamer <htchd7ji@live.com>
2019-05-14 08:39:48 -0400

    1st commit

---
+++ imports/ui/body.js
@@ -0,0 +1,46 @@
+import {Meteor} from 'meteor/meteor';
+import {Template} from 'meteor/templating';
+import { ReactiveDict} from 'meteor/reactive-dict';
+
+import {Tasks} from '../api/tasks';
+
+import './task.js';
+import './body.html';
+
+Template.body.onCreated(function bodyOnCreated(){
+	this.state = new ReactiveDict();
+});
+
+Template.body.helpers({
+	tasks(){
+		const instance = Template.instance();
+		if (instance.state.get('hide-completed')){
+			return Tasks.find({checked: {$ne:true}}, {sort:{createdAt:-1}});
+		}
+
+		return Tasks.find({}, {sort:{createdAt:-1}});
+	},
+	incompleteCount(){
+		return Tasks.find({checked:{$ne:true}}).count();
+	},
+});
+
+Template.body.events({
+	'submit .new-task'(event){
+		event.preventDefault();
+		const target = event.target;
+		const text = target.text.value;
+
+		Tasks.insert({
+			text,
+			createdAt:new Date(),
+			owner:Meteor.userId(),
+			username:Meteor.user().username,
+		});
+
+		target.text.value="";
+	},
+	'change .hide-completed input' (event, instance){
+		instance.state.set('hide-completed', event.target.checked);
+	},
+});
